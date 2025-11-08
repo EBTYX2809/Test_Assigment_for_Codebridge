3af0a89544e7c2ac0725ce64736600dc0154367b
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-08 13:45:32 +0200

    Added OnModelCreating to AppDbContext.

--- Test_Assigment_for_Codebridge/DataBase/AppDbContext.cs
+++ Test_Assigment_for_Codebridge/DataBase/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Test_Assigment_for_Codebridge.DataBase.Configurations;
 using Test_Assigment_for_Codebridge.Models;
 
 namespace Test_Assigment_for_Codebridge.DataBase;
@@ -8,4 +9,10 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<Dog> Dogs { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new DogConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
