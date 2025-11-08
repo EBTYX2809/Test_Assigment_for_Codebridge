6c17146f47eb9f99dfeecb0cd6e3379ddf4b1a90
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-08 20:25:56 +0200

    Changed sorting parameters.

--- Test_Assigment_for_Codebridge/Models/SortingAndPagination/DTOs/DogQueryDTO.cs
+++ Test_Assigment_for_Codebridge/Models/SortingAndPagination/DTOs/DogQueryDTO.cs
@@ -5,7 +5,10 @@ namespace Test_Assigment_for_Codebridge.Models.SortingAndPagination.DTOs;
 public class DogQueryDTO
 {    
     public int Page { get; set; } = 1;
-    public int PageSize { get; set; } = 10;    
-    public SortingAttribute SortBy { get; set; }
-    public SortingOrder OrderBy { get; set; }
+    public int PageSize { get; set; } = 10;
+    public string SortBy { get; set; } = "Id";
+    public string OrderBy { get; set; } = "Ascending";
+    // I decided to not using enums now because setting strings better for HTTP requests
+    // public SortingAttribute SortBy { get; set; } 
+    // public SortingOrder OrderBy { get; set; }
 }
