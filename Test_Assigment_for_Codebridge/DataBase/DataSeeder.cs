027772dffbe40394fc3ff4a1218f5c947e2e58b7
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-09 13:57:45 +0200

    Updated DataSeeder with async and added ClearDb method.

--- Test_Assigment_for_Codebridge/DataBase/DataSeeder.cs
+++ Test_Assigment_for_Codebridge/DataBase/DataSeeder.cs
@@ -4,15 +4,24 @@ namespace Test_Assigment_for_Codebridge.DataBase;
 
 public static class DataSeeder
 {
-    public static void Seed(IServiceProvider serviceProvider)
+    public static async Task Seed(IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-        dbContext.Dogs.AddRangeAsync(
+        await dbContext.Dogs.AddRangeAsync(
             new Dog { Name = "Neo", Color = "red & amber", TailLength = 22, Weight = 32 },
             new Dog { Name = "Jessy", Color = "black & white", TailLength = 7, Weight = 14 });
 
-        dbContext.SaveChangesAsync();
+        await dbContext.SaveChangesAsync();
+    }
+
+    public static async Task ClearDb(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        dbContext.Dogs.RemoveRange(dbContext.Dogs);
+        await dbContext.SaveChangesAsync();
     }
 }
