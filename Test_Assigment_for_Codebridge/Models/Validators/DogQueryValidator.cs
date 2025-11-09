cc7da03132a45ff620b5c35616bb34bb80fa6f7f
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-09 12:12:01 +0200

    Created validators for dog and query.

---
+++ Test_Assigment_for_Codebridge/Models/Validators/DogQueryValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Test_Assigment_for_Codebridge.Models.SortingAndPagination.DTOs;
+
+namespace Test_Assigment_for_Codebridge.Models.Validators;
+
+public class DogQueryValidator : AbstractValidator<DogQueryDTO>
+{
+    public DogQueryValidator()
+    {
+        RuleFor(d => d.Page)
+            .GreaterThan(0)
+            .WithMessage("Page number must be greater than 0.");
+
+        RuleFor(d => d.PageSize)
+            .GreaterThan(0)
+            .WithMessage("Page size must be greater than 0.");
+
+        RuleFor(d => d.SortBy)
+            .Must(sortBy => new[] { "Id", "Name", "Color", "TailLength", "Weight" }
+                .Contains(sortBy, StringComparer.OrdinalIgnoreCase))
+            .WithMessage("SortBy must be one of the following: Id, Name, Color, TailLength, Weight.");
+
+        RuleFor(d => d.OrderBy)
+            .Must(orderBy => new[] { "Ascending", "Descending" }
+                .Contains(orderBy, StringComparer.OrdinalIgnoreCase))
+            .WithMessage("OrderBy must be either 'Ascending' or 'Descending'.");
+    }
+}
---
+++ Test_Assigment_for_Codebridge/Models/Validators/DogValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace Test_Assigment_for_Codebridge.Models.Validators;
+
+public class DogValidator : AbstractValidator<Dog>
+{    
+    public DogValidator()
+    {      
+        RuleFor(d => d.Name)
+            .NotEmpty()
+            .WithMessage("Name cannot be empty.")
+            .MaximumLength(20)
+            .WithMessage("Name cannot be longer than 20 characters.");            
+
+        RuleFor(d => d.Color)
+            .NotEmpty()
+            .WithMessage("Color cannot be empty.")
+            .MaximumLength(20)
+            .WithMessage("Color cannot be longer than 20 characters.");
+
+        RuleFor(d => d.TailLength)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Tail length must be greater than 0.");
+
+        RuleFor(d => d.Weight)
+            .GreaterThan(0)
+            .WithMessage("Weight must be greater than 0.");
+    }
+}
