8b583fcbaec68984a59527e32c6afac85a639b9a
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-09 19:44:25 +0200

    Created first test.

---
+++ Tests/DogServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Assigment_for_Codebridge.DataBase;
+using Test_Assigment_for_Codebridge.Models;
+using Test_Assigment_for_Codebridge.Services;
+using Tests.DataBase;
+
+namespace Tests;
+
+public class DogServiceTests
+{
+    private readonly AppDbContext _dbContext;
+    private readonly DogService _dogService;
+
+    public DogServiceTests()
+    {
+        _dbContext = TestDbContextFactory.CreateInMemory();
+        _dogService = new DogService(_dbContext);
+    }
+
+    [Fact]
+    public async Task CreateGodTest()
+    {
+        // Arrange
+        var newDog = new Dog
+        {
+            Name = "Doggy",
+            Color = "brredown",
+            TailLength = 173,
+            Weight = 33
+        };
+        
+        // Act
+        await _dogService.CreateDogAsync(newDog);
+        var createdDog = await _dbContext.Dogs.FirstOrDefaultAsync(d => d.Id == newDog.Id);
+        
+        // Assert
+        Assert.NotNull(createdDog);
+        Assert.Equal(newDog.Name, createdDog.Name);
+        Assert.Equal(newDog.Color, createdDog.Color);
+        Assert.Equal(newDog.TailLength, createdDog.TailLength);
+        Assert.Equal(newDog.Weight, createdDog.Weight);
+    }
+        
+}
