3b0486d19537847a6ba3b28b32cfe1216ac37d63
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-09 19:44:05 +0200

    Created test data base.

---
+++ Tests/DataBase/TestDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Test_Assigment_for_Codebridge.DataBase;
+using Test_Assigment_for_Codebridge.Models;
+
+namespace Tests.DataBase;
+
+public static class TestDbContextFactory
+{
+    public static AppDbContext CreateInMemory()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        var dbContext = new AppDbContext(options);
+        dbContext.Database.EnsureCreated();
+
+        // Seed data
+        dbContext.Dogs.AddRange(
+            new Dog { Name = "Neo", Color = "red & amber", TailLength = 22, Weight = 32 },
+            new Dog { Name = "Jessy", Color = "black & white", TailLength = 7, Weight = 14 });
+        dbContext.SaveChanges();
+
+        return dbContext;
+    }
+}
