3c21ab20629f50e526e971d7cfbab3969e4b80b1
EBTYX2809 <ebtyx2809@gmail.com>
2025-11-09 15:15:25 +0200

    Made init migration.

---
+++ Test_Assigment_for_Codebridge/Migrations/20251109131441_Initialize.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test_Assigment_for_Codebridge.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "dogs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    color = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    tail_length = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    weight = table.Column<int>(type: "int", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dogs", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dogs_name",
+                table: "dogs",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dogs");
+        }
+    }
+}
